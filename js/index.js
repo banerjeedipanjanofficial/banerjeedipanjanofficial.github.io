d02fbfe16c9797bec0a3f93035bf6e256dfd60bc
Dipanjan Banerjee <dipanjan04banerjee@gmail.com>
2024-07-17 02:20:13 +0530

    Updated Portfolio page

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const ul =document.getElementsByTagName('ul')[0];
-const nav =document.getElementsByTagName('nav')[0];
-const bar=document.getElementsByClassName('fa-bars')[0];
-
-bar.addEventListener('click',()=>{
-    ul.classList.toggle('active');
-    nav.classList.toggle('active');
-})
